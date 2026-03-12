495198589a80b3fd6ba80e82aa171bbfc271ef81
金色的诗岸 <91466399+K-Summer@users.noreply.github.com>
2026-03-13 04:56:12 +0800

    修改main提示

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
-    print("Hello from qwen3-tts-local!")
-
+    print("Hello from qwen3-tts-api!\n")
+    print("运行下面的命令安装依赖:")
+    commands = [
+        "uv pip install -U qwen3-tts",
+        "pip install -e .",
+        "uv pip install https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.7.12/flash_attn-2.8.3%2Bcu130torch2.10-cp314-cp314-win_amd64.whl",
+        "uv pip install torch torchvision torchaudio -f https://mirrors.aliyun.com/pytorch-wheels/cu130/",
+    ]
+    for cmd in commands:
+        print(f"  {cmd}")
 
 if __name__ == "__main__":
     main()
